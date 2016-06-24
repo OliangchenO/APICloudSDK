1cd4f9fd9ddc558a0efc7177d9a7901127fcfa7e
OliangchenO <liang_chen@aliyun.com>
2016-06-24 09:37:58 +0800

    修改验证图片

--- APICloudSDK/src/com/taobao/module/AliBaBa.java
+++ APICloudSDK/src/com/taobao/module/AliBaBa.java
@@ -24,16 +24,10 @@ public class AliBaBa extends UZModule {
             @Override
             public void onSuccess() {
             	JSONObject ret = new JSONObject();
-            	JSONObject err = new JSONObject();
             	try {
 					ret.put("msg", "初始化成功");
 					moduleContext.success(ret, true);
 				} catch (JSONException e) {
-					try {
-						err.put("msg", e.getMessage());
-						moduleContext.error(ret, err, true);
-					} catch (JSONException e1) {
-					}
 				}
             }
  
@@ -41,12 +35,11 @@ public class AliBaBa extends UZModule {
             public void onFailure(int code, String message) {
             	JSONObject ret = new JSONObject();
             	JSONObject err = new JSONObject();
-            	
             	try {
 					err.put("code", code);
 					err.put("msg", message);
-				} catch (JSONException e) {
 					moduleContext.error(ret, err, true);
+				} catch (JSONException e) {
 				}
             }
  
